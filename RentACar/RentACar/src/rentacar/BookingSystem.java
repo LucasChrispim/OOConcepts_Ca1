4cb04916ee43440a47f35a02714c1e34e4efc839
Lucas <73845967+LucasChrispim@users.noreply.github.com>
2022-11-05 22:50:47 +0000

    Update BookingSystem.java

--- RentACar/RentACar/src/rentacar/BookingSystem.java
+++ RentACar/RentACar/src/rentacar/BookingSystem.java
@@ -2,40 +2,58 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package rentacar;
+package ooc.yoursolution;
 
 import java.io.BufferedReader;
 import java.io.IOException;
-import ooc.yoursolution.BookingSystemInterface;
-import ooc.yoursolution.BookingSystemInterface;
-import ooc.yoursolution.Car;
-import ooc.yoursolution.CarInterface;
-import ooc.yoursolution.RentACarInterface;
+import static java.lang.Integer.parseInt;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import ooc.enums.Make;
 
 /**
- *
- * @author gusta
+ * @author Gustavo,Lucas
  */
-public class BookingSystem implements BookingSystemInterface {
-
-    public BookingSystem() {
-    }
-
+public class BookingSystem implements BookingSystemInterface{
+        
     @Override
-    public RentACarInterface setupRentACar(BufferedReader in) throws IOException {
+    public RentACarInterface setupRentACar(BufferedReader in) throws IOException{
+        
+        Scanner text = new Scanner(in);
+        
+        RentACarInterface carsToRent = new RentACar();
+        List<CarInterface> cars = new ArrayList<CarInterface>();
+        
+        
         String title = in.readLine();
+        carsToRent.setName(title);
         String content = "";
         
-        CarInterface car= new Car();
-        
-        for(int n = 1; n<= 5; n++){
-            String content = in.readLine();
-            String[] line = content.split(":");
-            String make = [0];
-            String rate1 = line[1];
-            int rate2 = parseInt(rate1);
+        int counter = 0;
+        while(in.ready()){
+            //System.out.println(title);
+            content = in.readLine();
+            //System.out.println(content);
+               
+            String[] parts = content.split(":");
+            
+            int id = counter + 1;
+            String rateString =  parts[1];
+            int intRate = parseInt(rateString);   //convert rateString to int
+            double rate = intRate;                  //convert Rate to double
+            Make make = Make.valueOf(parts[0]);
+            int number = parseInt(parts[2]);
+            Car car = new Car(id, rate, make, number);
+            cars.add(car);
+            counter++;
+            content = title;
+            //System.out.println(Arrays.toString(parts));
         }
-        return null;
-      }
-    
+        carsToRent.setCars(cars);
+        return carsToRent;
+        
+    }
+
 }
