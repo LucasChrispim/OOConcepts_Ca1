97957faa5d88a6f3be08a5e1d64fe4581dac3ffd
GusLambert <100359649+GusLambert@users.noreply.github.com>
2022-11-05 22:01:29 +0000

    Update Month.java

--- RentACar/RentACar/RentACar/src/ooc/enums/Month.java
+++ RentACar/RentACar/RentACar/src/ooc/enums/Month.java
@@ -1,15 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package ooc.enums;
 
-/**
- * THIS IS SOME LEGACY CODE THAT IS PART OF THE SYSTEM AND YOU HAVE TO USE IT.
- *
- * @author apont
- */
+
 public enum Month {
     JANUARY(31),
     FEBRUARY(28),
